d4c5be9f22eeb057aa594d380903e8c2558601ca
ZickZenni <zickzenniyt@gmail.com>
2026-03-23 19:13:06 +0100

    Disable indentation for the configuration

--- neoforge/src/main/java/com/zickzenni/anarchium/AnarchiumMod.java
+++ neoforge/src/main/java/com/zickzenni/anarchium/AnarchiumMod.java
@@ -2,6 +2,9 @@ package com.zickzenni.anarchium;
 
 import com.electronwill.nightconfig.core.ConfigSpec;
 import com.electronwill.nightconfig.core.file.FileConfig;
+import com.electronwill.nightconfig.core.io.IndentStyle;
+import com.electronwill.nightconfig.core.io.WritingMode;
+import com.electronwill.nightconfig.toml.TomlWriter;
 import com.zickzenni.anarchium.client.network.*;
 import com.zickzenni.anarchium.network.*;
 import com.zickzenni.anarchium.registry.EffectRegistry;
@@ -16,16 +19,23 @@ import net.neoforged.fml.common.Mod;
 import net.neoforged.neoforge.network.event.RegisterPayloadHandlersEvent;
 import net.neoforged.neoforge.registries.DeferredRegister;
 
+import java.io.File;
+
 @Mod(Constants.MOD_ID)
 public class AnarchiumMod
 {
     private static final String NETWORK_VERSION = "1";
     private static final DeferredRegister<SoundEvent> SOUND_EVENTS =
             DeferredRegister.create(BuiltInRegistries.SOUND_EVENT, Constants.MOD_ID);
-    public static final FileConfig CONFIG = FileConfig.of("./config/" + Constants.MOD_ID + "-common.toml");
+
+    private static final File CONFIG_FILE = new File("./config/" + Constants.MOD_ID + "-common.toml");
+    public static final FileConfig CONFIG = FileConfig.of(CONFIG_FILE);
+    private static final TomlWriter CONFIG_WRITER = new TomlWriter();
 
     public AnarchiumMod(IEventBus bus)
     {
+        CONFIG_WRITER.setIndent(IndentStyle.NONE);
+
         SoundRegistry.DISPATCH_EFFECT_SOUND =
                 SOUND_EVENTS.register("dispatch_effect", SoundEvent::createVariableRangeEvent);
         SoundRegistry.ZEUS_EFFECT_SOUND = SOUND_EVENTS.register("zeus_effect", SoundEvent::createVariableRangeEvent);
@@ -40,7 +50,7 @@ public class AnarchiumMod
         AnarchiumServer.TIMER_DURATION.configure(specs);
 
         specs.correct(CONFIG);
-        CONFIG.save();
+        CONFIG_WRITER.write(CONFIG, CONFIG_FILE, WritingMode.REPLACE);
     }
 
     @SuppressWarnings("unused")
