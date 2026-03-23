9555e85193ef1f9df29d10db7cfe8f1416138375
ZickZenni <zickzenniyt@gmail.com>
2026-03-23 17:51:04 +0100

    Remove old project code

--- src/main/java/com/zickzenni/anarchium/client/gui/Overlay.java
+++
@@ -1,8 +0,0 @@
-package com.zickzenni.anarchium.client.gui;
-
-import net.minecraft.client.gui.GuiGraphics;
-
-public interface Overlay
-{
-    void render(GuiGraphics graphics, float deltaTime);
-}
--- src/main/java/com/zickzenni/anarchium/util/function/Double3Consumer.java
+++
@@ -1,7 +0,0 @@
-package com.zickzenni.anarchium.util.function;
-
-@FunctionalInterface
-public interface Double3Consumer
-{
-    void accept(double x, double y, double z);
-}
